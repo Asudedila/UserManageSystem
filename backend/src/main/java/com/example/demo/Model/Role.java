9ac9d750126f4b6af59516f1ee7d3fc60b24e104
Asudedila <126553674+Asudedila@users.noreply.github.com>
2024-07-22 09:31:29 +0300

    Update Role.java

--- backend/src/main/java/com/example/demo/Model/Role.java
+++ backend/src/main/java/com/example/demo/Model/Role.java
@@ -2,7 +2,6 @@ package com.example.demo.Model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-//date ,user silinsede login veriyor onu kontrol et,foreign key
 @Entity
 @Table(name="role")
 @Data
